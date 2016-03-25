8ddc59931d6b995163fef0452027bc49a0591014
prajesh <prajeshravindran@gmail.com>
2016-03-24 22:21:01 -0500

    108. Convert Sorted Array to Binary Search Tree

--- LeetCodeTrees/src/Solution.java
+++ LeetCodeTrees/src/Solution.java
@@ -2,6 +2,7 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
+import java.util.Stack;
 
 
 public class Solution {
@@ -73,4 +74,49 @@ public class Solution {
       }
       return lca;
   }
+    
+    public TreeNode lowestCommonAncestor2(TreeNode root, TreeNode p, TreeNode q) {
+      while ((root.val - p.val) * (root.val - q.val) > 0)
+          root = p.val < root.val ? root.left : root.right;
+      return root;
+}
+       
+    /*
+     * 144. Binary Tree Preorder Traversal
+     */
+        public List<Integer> preorderTraversal(TreeNode root) {
+          
+          List<Integer> l = new ArrayList<Integer>();
+          Stack<TreeNode> st = new Stack<TreeNode>();
+          if(root!=null)st.push(root);
+          while(!st.isEmpty())
+          {
+              TreeNode node = st.pop();
+          
+              while(node!=null)
+              {
+                   l.add(node.val);
+                   if(node.right!=null)st.push(node.right);
+                   node = node.left;
+              }
+          }
+          return l;
+      }
+        
+/*
+ * 108. Convert Sorted Array to Binary Search Tree
+ */
+        private TreeNode createTree(int[] a, int l, int r)
+        {
+            if(l>r||l<0||r>=a.length)return null;
+            int mid = l + (r-l)/2;
+            TreeNode node = new TreeNode(a[mid]);
+            node.left = createTree(a,l,mid-1);
+            node.right = createTree(a,mid+1,r);
+            return node;
+        }
+        public TreeNode sortedArrayToBST(int[] nums) {
+            return createTree(nums,0,nums.length-1);
+            
+        }        
 }
