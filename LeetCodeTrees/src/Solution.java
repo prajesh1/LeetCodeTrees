87adf4a1f2c5ef8432ea374a47711a4fd8e029b8
prajesh <prajeshravindran@gmail.com>
2016-04-03 16:18:03 -0500

    199. Binary Tree Right Side View

--- LeetCodeTrees/src/Solution.java
+++ LeetCodeTrees/src/Solution.java
@@ -122,6 +122,7 @@ public class Solution {
         
  /*
   * 105. Construct Binary Tree from Preorder and Inorder Traversal
+  * Improved solution with creating a hashmap for in[]
   */
         private int i=0;
         private TreeNode built(int[] pr, int[] in,int st, int end )
@@ -144,5 +145,92 @@ public class Solution {
         }
         public TreeNode buildTree(int[] preorder, int[] inorder) {
             return built(preorder,inorder,0,preorder.length-1);
+        }
+        
+        
+          private List<String> paths= new ArrayList<String>();
+          public List<String> binaryTreePaths(TreeNode root) {
+              if(root==null) return paths;
+              dfs(root,null);
+              return paths;
+          }
+          private void dfs(TreeNode node, String s)
+          {
+              if(node==null){
+                  if(s!=null)paths.add(s);
+              }
+              dfs(node.left,s+node.val);
+              dfs(node.right,s+node.val);
+          }
+          
+          private Boolean[][] visited;
+          private int rows, cols;
+          public int numIslands(char[][] grid) {
+              if(grid==null) return 0;
+               rows= grid.length;cols= grid[0].length; int count=0;;
+              visited = new Boolean[rows][cols];
+              for(int i=0;i<rows;i++)
+              for(int j=0;j<cols;j++)
+              {
+                  if(!visited[i][j])
+                  dfs(i,j);
+                  count++;
+              }
+              return count;
+          }
+          private void dfs(int i, int j)
+          {
+              this.visited[i][j] = true;
+              for(int[] node: adj(i,j))
+                if(!visited[node[0]][node[1]])
+                  dfs(node[0],node[1]);
+          }
+          private List<int[]> adj(int i, int j)
+          {
+           List<int[]> a = new ArrayList<int[]>();
+           if(i<rows-1)
+           {
+             int[] right = {i+1,j};
+             a.add(right);
+           }
+           if(i>0)
+           {
+             int[] left = {i-1,j};
+             a.add(left);
+           }
+           if(j<cols-1)
+           {
+             int[] top = {i,j-1};
+             a.add(top);
+           }
+           if(j>0)
+           {
+             int[] bot = {i,j+1};
+             a.add(bot);
+           }
+           return a;
+          }
+          /*
+           * 199. Binary Tree Right Side View
+           */
+          public List<Integer> rightSideView(TreeNode root) {
+            if(root==null)return new ArrayList<Integer>();
+            List<Integer> view = new ArrayList<Integer>();
+            Queue<TreeNode> q1 = new LinkedList<TreeNode>();
+            q1.add(root);
+            while(!q1.isEmpty())
+            {
+                Queue<TreeNode> q2 = new LinkedList<TreeNode>();
+                TreeNode last =null;
+                while(!q1.isEmpty())
+                {
+                    last = q1.remove();
+                    if(last.left!=null) q2.add(last.left);
+                    if(last.right!=null) q2.add(last.right);
+                }
+                view.add(last.val);
+                q1 = q2;
+            }
+            return view;
         }
 }
